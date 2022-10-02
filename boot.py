fe9020095f5290ad7748a4ea3810cb5ecc686851
Krystof Woldrich <krystof51@gmail.com>
2022-10-02 18:12:50 +0200

    Fix lint using black

--- boot.py
+++ boot.py
@@ -2,9 +2,9 @@
 # With changes but the core remains the same
 
 try:
-  import usocket as socket
+    import usocket as socket
 except:
-  import socket
+    import socket
 import network
 from machine import Pin
 import esp
@@ -14,49 +14,54 @@ from umqttsimple import MQTTClient
 import ubinascii
 import machine
 
+
 def get_config():
     config = {}
-    file = open('config', 'r')
+    file = open("config", "r")
     for line in file:
-        if (len(line.strip()) > 0):
-            raw = line.split('=', 1)
-            config[raw[0].strip()]=raw[1].strip()
+        if len(line.strip()) > 0:
+            raw = line.split("=", 1)
+            config[raw[0].strip()] = raw[1].strip()
     file.close()
     return config
 
+
 def save_config(config):
-    file = open('config', 'w')
-    raw = ''
+    file = open("config", "w")
+    raw = ""
     for key in config:
-        raw += key + '=' + config[key] + '\n'
+        raw += key + "=" + config[key] + "\n"
     file.write(raw)
     file.close()
 
+
 def connect_wifi(ssid, password):
-  wlan = network.WLAN(network.STA_IF)
-  wlan.active(True)
-  if not wlan.isconnected():
-      print('connecting to network...')
-      wlan.connect(ssid, password)
-      while not wlan.isconnected():
-          pass
-  print('network config:', wlan.ifconfig())
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print("connecting to network...")
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print("network config:", wlan.ifconfig())
+
 
 def setup_mqtt(config):
     id = machine.unique_id()
     client_id = ubinascii.hexlify(id)
-    config['client_id'] = client_id
+    config["client_id"] = client_id
+
 
 esp.osdebug(None)
 gc.collect()
 
 config = get_config()
-sensor = dht.DHT22(Pin(int(config['sensor'])))
+sensor = dht.DHT22(Pin(int(config["sensor"])))
 
-ssid = config['ssid']
-password = config['password']
+ssid = config["ssid"]
+password = config["password"]
 connect_wifi(ssid, password)
 
 setup_mqtt(config)
 
-print('Boot complete')
+print("Boot complete")
--- main.py
+++ main.py
@@ -8,25 +8,30 @@ import time
 import machine
 from uselect import select
 
+
 def read_sensor():
     temp = hum = 0
     try:
         sensor.measure()
         temp = sensor.temperature()
         hum = sensor.humidity()
-        if (isinstance(temp, float) and isinstance(hum, float)) or (isinstance(temp, int) and isinstance(hum, int)):         
+        if (isinstance(temp, float) and isinstance(hum, float)) or (
+            isinstance(temp, int) and isinstance(hum, int)
+        ):
             hum = round(hum, 2)
-            return({
-                'temperature': temp,
-                'humidity': hum,
-            })
+            return {
+                "temperature": temp,
+                "humidity": hum,
+            }
         else:
-            return('Invalid sensor readings.')
+            return "Invalid sensor readings."
     except OSError as e:
-        return('Failed to read sensor.')
+        return "Failed to read sensor."
+
 
 def web_page(data):
-  html = """<!DOCTYPE HTML><html>
+    html = (
+        """<!DOCTYPE HTML><html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
@@ -52,64 +57,72 @@ def web_page(data):
   <p>
     <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
     <span class="dht-labels">Temperature</span> 
-    <span>"""+str(data['temperature'])+"""</span>
+    <span>"""
+        + str(data["temperature"])
+        + """</span>
     <sup class="units">&deg;C</sup>
   </p>
   <p>
     <i class="fas fa-tint" style="color:#00add6;"></i> 
     <span class="dht-labels">Humidity</span>
-    <span>"""+str(data['humidity'])+"""</span>
+    <span>"""
+        + str(data["humidity"])
+        + """</span>
     <sup class="units">%</sup>
   </p>
 </body>
 </html>"""
-  return html
+    )
+    return html
+
 
 def restart():
-  print('Failed to connect to MQTT broker. Reconnecting...')
-  time.sleep(10)
-  machine.reset()
+    print("Failed to connect to MQTT broker. Reconnecting...")
+    time.sleep(10)
+    machine.reset()
+
 
 def setup_mqtt_client(config):
-    mqtt_server = config['mqtt_server']
-    client_id = config['client_id']
-    print('%s %s' % (mqtt_server, client_id))
+    mqtt_server = config["mqtt_server"]
+    client_id = config["client_id"]
+    print("%s %s" % (mqtt_server, client_id))
     client = MQTTClient(client_id, mqtt_server, port=1883, keepalive=60)
     try:
         client.connect()
-        print('Connected to %s MQTT broker' % (mqtt_server))
+        print("Connected to %s MQTT broker" % (mqtt_server))
         return client
     except OSError as e:
         restart()
 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
+s.bind(("", 80))
 s.listen(5)
 
 client = setup_mqtt_client(config)
 
 last_send = 0
 while True:
-  sensor_readings = read_sensor()
+    sensor_readings = read_sensor()
 
-  if (time.time() - last_send) > int(config['message_interval_s']):
-    print('Sending message')
-    client.publish('test/temp', str(sensor_readings['temperature']))
-    client.publish('test/hum', str(sensor_readings['humidity']))
-    last_send = time.time()
+    if (time.time() - last_send) > int(config["message_interval_s"]):
+        print("Sending message")
+        client.publish("test/temp", str(sensor_readings["temperature"]))
+        client.publish("test/hum", str(sensor_readings["humidity"]))
+        last_send = time.time()
 
-  r, w, err = select((s,), (), (), 1)
-  if r:
-    for readable in r:
-      conn, addr = s.accept()
-      try:
-        print('Got a connection from %s' % str(addr))
-        request = conn.recv(1024)
-        response = web_page(sensor_readings)
-        conn.send('HTTP/1.1 200 OK\n')
-        conn.send('Content-Type: text/html\n')
-        conn.send('Connection: close\n\n')
-        conn.sendall(response)
-        conn.close()
-      except OSError as e:
-          pass
+    r, w, err = select((s,), (), (), 1)
+    if r:
+        for readable in r:
+            conn, addr = s.accept()
+            try:
+                print("Got a connection from %s" % str(addr))
+                request = conn.recv(1024)
+                response = web_page(sensor_readings)
+                conn.send("HTTP/1.1 200 OK\n")
+                conn.send("Content-Type: text/html\n")
+                conn.send("Connection: close\n\n")
+                conn.sendall(response)
+                conn.close()
+            except OSError as e:
+                pass
